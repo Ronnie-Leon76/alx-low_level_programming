22781894f6832fbd1f6fc3bbfbe7b578928eee3f
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-08 11:21:11 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ *
+ *  * main - Entry point
+ *
+ *  * puts prints the string to the terminal
+ *
+ *  * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
