8fbdc03871f5f82904a0e70e2a3e7d2cb4813982
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-08 11:32:31 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 /**
- *
  *  main - Entry point
- *
  *  puts prints the string to the terminal
- *
- *  Return: Always 0 (Success)
- *
- * 
+ *  Return: Always 0 (Success) 
  */
 
 int main(void)
