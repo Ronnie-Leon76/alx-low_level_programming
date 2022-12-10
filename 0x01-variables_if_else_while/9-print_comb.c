54868e7fe44cd456dff161bbbf28d14d62a366f3
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-10 02:08:54 -0800

    Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,20 +9,13 @@
 int main(void)
 {
 	int i;
-	int j;
-
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 1 ; j < 10; j++)
 	{
-		for (j = i + 1 ; j < 10; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
