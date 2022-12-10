7f4a604a991c3e48357fabaa16ce2c0593634726
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-10 00:49:23 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry file
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10 ; i++)
+		putchar(i + '0');
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
