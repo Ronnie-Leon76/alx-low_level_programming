3f046ee7122efc10c37b381a8bccdb48af03b6ce
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-10 11:08:51 -0800

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry file
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art");
+	return (0);
+}
