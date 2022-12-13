faac2f81f7d22ead995969de0a055652862a11bd
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-13 10:14:44 -0800

    Defined _putchar method prototype in main.h and added the function implementation

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /*
  * main - Entry file
@@ -10,11 +11,14 @@
 
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
-
-	for(i=0; i < 8; i++)
-		putchar(c[i]);
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
