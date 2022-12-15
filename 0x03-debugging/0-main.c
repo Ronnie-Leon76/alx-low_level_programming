6397b3f05f56a1e0bc2fda0c2d520f006c0ec99e
Ronnie-Leon76 <ronleon76@gmail.com>
2022-12-15 07:01:02 -0800

    removed extra white spaces

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include "main.h"
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
-
+ * Return: Always 0 (Success)*/
 int main(void)
 {
 	int n;
