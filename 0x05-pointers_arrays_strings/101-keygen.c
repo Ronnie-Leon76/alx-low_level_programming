964beb71c1a0997c218d06840a508b166645175b
Ronnie Leon <60542881+Ronnie-Leon76@users.noreply.github.com>
2022-12-21 07:27:56 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,11 +14,14 @@ int main(void)
 
 	srand(time(NULL));
 	sum = 0;
+	// 2772 is the sum of all ASCII values of the password
 	for (i = 0; i < 2772; i++)
 	{
+		// get the mod of 128 to get a valid ASCII value
 		pass[i] = rand() % 128;
 		sum += pass[i];
 	}
+	// subtract the difference between the sum and 2772 from the last character
 	pass[i - 1] -= sum - 2772;
 	for (i = 0; pass[i] != '\0'; i++)
 		printf("%c", pass[i]);
