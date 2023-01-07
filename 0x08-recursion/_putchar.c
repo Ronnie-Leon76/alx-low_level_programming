847c944d58c18bfcb0dd59905908180e872c4fd2
Ronnie-Leon76 <ronleon76@gmail.com>
2023-01-06 22:17:14 -0800

    _puts_recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchat - write the character c to the stdout
+ * @c: The character to print
+ *
+ * Return: On Success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
