fcdb2f65228dfcdb272fb6a05faa1b329796ba36
Ronnie-Leon76 <ronleon76@gmail.com>
2023-01-14 03:32:17 -0800

    number of coins from a total amount

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the minimum number of coins to make change for an amount of money
+ * main - prints the minimum number of coins
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int den[] = {25, 10, 5, 2, 1};
 
 	coins = 0;
-	if(argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	i = 0;        
+	i = 0;
 	while (sum > 0)
 	{
 		j = sum / den[i];
