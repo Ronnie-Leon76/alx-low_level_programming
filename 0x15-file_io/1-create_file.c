2f82298ae8b059e74c6097195257e141fe932fff
Ronnie-Leon76 <ronleon76@gmail.com>
2023-02-11 04:35:36 -0800

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
