1565c4465395d0d0f240ce10b24800b4f86e85ba
Ronnie-Leon76 <ronleon76@gmail.com>
2023-02-11 05:40:49 -0800

    cp from_file to_file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, r, w;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to %s", argv[0]);
+	exit(97);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s %s", argv[1], argv[0]);
+	exit(98);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s %s", argv[2], argv[0]);
+	exit(99);
+	while ((r = read(fd1, buf, 1024)) > 0)
+	{
+		w = write(fd2, buf, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s %s", argv[2], argv[0]);
+		exit(99);
+	}
+	if (r == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s %s", argv[1], argv[0]), exit(98);
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d %s", fd1, argv[0]);
+	exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d %s", fd2, argv[0]);
+	exit(100);
+	return (0);
+}
