62682db6af9f966777ebe4551ab8adc4631e51b9
Ronnie-Leon76 <ronleon76@gmail.com>
2023-04-06 19:42:19 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	if (!table)
 		return NULL;
 	table->size = size;
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
