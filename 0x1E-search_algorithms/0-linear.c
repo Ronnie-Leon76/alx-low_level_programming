6de1427c53a4903902d174ae84574e09c36b9c9c
Ronnie-Leon76 <ronleon76@gmail.com>
2023-10-11 02:10:39 +0300

    feat: Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (array)
-    {
-        while (i < size)
-        {
-            printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-            if (array[i] == value)
-                return (i);
-            i++;
-        }
-    }
-    return (-1);
+	if (array)
+	{
+		while (i < size)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+			i++;
+		}
+	}
+	return (-1);
 }
